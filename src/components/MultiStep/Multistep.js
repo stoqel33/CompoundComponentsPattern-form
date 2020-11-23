d6e6aac61a7512c076b76f29d18b5a64236543fb
stoqel33 <stochel202@interia.pl>
2020-11-23 20:14:07 +0100

    use useContext in order to pass informations about pages and functions, add logic for controls buttons

--- src/components/MultiStep/Multistep.js
+++ src/components/MultiStep/Multistep.js
@@ -1,21 +1,66 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 
-const Page = ({ children }) => {
-  return <div>{children}</div>;
+const WizardContext = React.createContext({
+  currentPage: 1,
+  changePage: () => {},
+  pages: [],
+  updatePages: () => {},
+});
+
+const Page = ({ children, pageIndex }) => {
+  const { currentPage, updatePages } = useContext(WizardContext);
+
+  useEffect(() => {
+    updatePages(pageIndex);
+  });
+  return currentPage === pageIndex ? children : null;
 };
 
 const Controls = () => {
+  const { currentPage, changePage, pages } = useContext(WizardContext);
   return (
     <div>
-      <button>Previous</button>
-      <button>Next</button>
-      <button>Submit</button>
+      <button
+        className="button is-info"
+        disabled={currentPage === 0}
+        onClick={() => changePage(currentPage - 1)}
+      >
+        Previous
+      </button>
+      <button
+        className="button is-info"
+        disabled={currentPage === pages.length - 1}
+        onClick={() => changePage(currentPage + 1)}
+      >
+        Next
+      </button>
+      {currentPage === pages.length - 1 ? (
+        <button className="button is-success">Submit</button>
+      ) : null}
     </div>
   );
 };
 
 const Wizard = ({ children }) => {
-  return <div>{children}</div>;
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pages, setPages] = useState([]);
+  const changePage = (newPage) => setCurrentPage(newPage);
+  const updatePages = (page) => {
+    if (!pages.includes(page)) setPages([...pages, page]);
+  };
+
+  return (
+    <WizardContext.Provider
+      value={{
+        currentPage,
+        changePage,
+        pages,
+        updatePages,
+      }}
+    >
+      {children}
+    </WizardContext.Provider>
+  );
 };
 
 export { Page, Controls, Wizard };
