d257b8faa5d8e3c0ef2094b342138b93c20c4949
stoqel33 <stochel202@interia.pl>
2020-11-23 20:11:35 +0100

    add style, some initial questions, and use multistep page and controls

--- src/views/Personal/Personal.js
+++ src/views/Personal/Personal.js
@@ -1,13 +1,54 @@
 import React from "react";
+import styled from "./Personal.module.scss";
 import * as MultiStep from "../../components/MultiStep/Multistep";
 
 const Personal = () => {
   return (
-    <MultiStep.Wizard>
-      <MultiStep.Page>Question 1</MultiStep.Page>
-      <MultiStep.Page>Question 2</MultiStep.Page>
-      <MultiStep.Controls />
-    </MultiStep.Wizard>
+    <div className={styled.wrapper}>
+      <MultiStep.Wizard>
+        <MultiStep.Page pageIndex={0}>
+          <div class={styled.innerWrapper}>
+            <p class="subtitle is-3">You will see some personal questions</p>
+            <p class="subtitle is-5">Let's get started!</p>
+          </div>
+        </MultiStep.Page>
+        <MultiStep.Page pageIndex={1}>
+          <div className="box">
+            <p class="subtitle is-3">What is your name?</p>
+            <div class="control">
+              <input
+                class="input is-rounded"
+                type="text"
+                placeholder="Your name..."
+              ></input>
+            </div>
+          </div>
+        </MultiStep.Page>
+        <MultiStep.Page pageIndex={2}>
+          <div className="box">
+            <p class="subtitle is-3">What is your phone number?</p>
+            <div class="control">
+              <input
+                class="input is-rounded"
+                type="tel"
+                placeholder="Your phone number..."
+              ></input>
+            </div>
+          </div>
+        </MultiStep.Page>
+        <MultiStep.Page pageIndex={3}>
+          <div className="box">
+            <p class="subtitle is-3">What is your date of birth?</p>
+            <div class="control">
+              <input class="input is-rounded" type="date"></input>
+            </div>
+          </div>
+        </MultiStep.Page>
+        <div className={styled.innerWrapper}>
+          <MultiStep.Controls />
+        </div>
+      </MultiStep.Wizard>
+    </div>
   );
 };
 
