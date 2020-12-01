8e3afec7b1ecea7885402c8e009190a9dad79c0e
stoqel33 <stochel202@interia.pl>
2020-12-01 18:24:36 +0100

    refactoring code

--- src/views/Personal/Personal.js
+++ src/views/Personal/Personal.js
@@ -2,6 +2,30 @@ import React, { useReducer } from "react";
 import styled from "./Personal.module.scss";
 import * as MultiStep from "../../components/MultiStep/Multistep";
 
+const questions = [
+  {
+    id: 1,
+    text: "What is your name?",
+    name: "name",
+    type: "text",
+    placeholder: "Your name...",
+  },
+  {
+    id: 2,
+    text: "What is your phone number?",
+    name: "phoneNumber",
+    type: "tel",
+    placeholder: "Your phone number...",
+  },
+  {
+    id: 3,
+    text: "What is your date of birth?",
+    name: "dateOfBirth",
+    type: "date",
+    placeholder: "",
+  },
+];
+
 const Personal = () => {
   const [inputData, setInputData] = useReducer(
     (state, newState) => ({ ...state, ...newState }),
@@ -28,51 +52,44 @@ const Personal = () => {
             <p className="subtitle is-5">Let's get started!</p>
           </div>
         </MultiStep.Page>
-        <MultiStep.Page pageIndex={1}>
-          <div className="box">
-            <p className="subtitle is-3">What is your name?</p>
-            <div className="control">
-              <input
-                name="name"
-                value={inputData.name}
-                onChange={setAnswer}
-                className="input is-rounded"
-                type="text"
-                placeholder="Your name..."
-              ></input>
-            </div>
-          </div>
-        </MultiStep.Page>
-        <MultiStep.Page pageIndex={2}>
-          <div className="box">
-            <p className="subtitle is-3">What is your phone number? </p>
-            <p className="subtitle is-5">( 9 number characters )</p>
-            <div className="control">
-              <input
-                name="phoneNumber"
-                value={inputData.phoneNumber}
-                onChange={setAnswer}
-                className="input is-rounded"
-                type="tel"
-                placeholder="Your phone number..."
-              ></input>
-            </div>
-          </div>
-        </MultiStep.Page>
-        <MultiStep.Page pageIndex={3}>
-          <div className="box">
-            <p className="subtitle is-3">What is your date of birth?</p>
-            <div className="control">
-              <input
-                name="dateOfBirth"
-                value={inputData.dateOfBirth}
-                onChange={setAnswer}
-                className="input is-rounded"
-                type="date"
-              ></input>
-            </div>
-          </div>
-        </MultiStep.Page>
+        {questions.map((question) => {
+          let value;
+
+          switch (question.name) {
+            case "name":
+              value = inputData.name;
+              break;
+            case "phoneNumber":
+              value = inputData.phoneNumber;
+              break;
+            case "dateOfBirth":
+              value = inputData.dateOfBirth;
+              break;
+            default:
+              break;
+          }
+
+          return (
+            <MultiStep.Page pageIndex={question.id} key={question.id}>
+              <div className="box">
+                <p className="subtitle is-3">{question.text}</p>
+                {question.type === "tel" && (
+                  <p className="subtitle is-5">( 9 number characters )</p>
+                )}
+                <div className="control">
+                  <input
+                    name={question.name}
+                    value={value}
+                    onChange={setAnswer}
+                    className="input is-rounded"
+                    type={question.type}
+                    placeholder={question.placeholder}
+                  ></input>
+                </div>
+              </div>
+            </MultiStep.Page>
+          );
+        })}
         <div className={styled.innerWrapper}>
           <MultiStep.Controls data={inputData} />
         </div>
