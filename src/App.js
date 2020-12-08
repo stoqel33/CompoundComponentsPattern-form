26ac53d84228fad2d631d64f7550c48a840e4b60
stoqel33 <stochel202@interia.pl>
2020-12-08 10:41:31 +0100

    use path hobbies and component hobbies in componnt app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Hobbies from "./views/Hobbies/Hobbies";
 import Personal from "./views/Personal/Personal";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <Header />
       <Route path="/personal" component={Personal} />
+      <Route path="/hobbies" component={Hobbies} />
     </Router>
   );
 };
